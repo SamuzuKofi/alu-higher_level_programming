538cfcb3f2c2ba208e674ac1bd6049d9009764d8
terrydestroye <s.amuzu@alustudent.com>
2025-02-18 00:28:20 +0200

    4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-import sys
-import importlib.util
+import hidden_4
 
 if __name__ == "__main__":
-    module_name = "hidden_4"
-    module_path = "~/Desktop/Git_repos/alu-higher_level_programming/python-import_modules"
-
-    spec = importlib.util.spec_from_file_location(module_name, module_path)
-    hidden_4 = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(hidden_4)
 
     names = dir(hidden_4)
-
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)

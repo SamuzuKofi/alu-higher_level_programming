177d168e196e0c0bfc844d676a76ae15335e21b0
terrydestroye <s.amuzu@alustudent.com>
2025-02-17 19:25:44 +0200

    2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ = "__main__":
+    acc_arg = sys.argv[1:]
+    arg_len = len(argv)
+
+    print("{} arguments{}{}".format(arg_len, "" if arg_len == 1 else "s", ":" if arg_len > 0 else "."))
+
+    for i, arg in enumerat(acc_arg, 1):
+        print("{}: {}".format(i, arg))

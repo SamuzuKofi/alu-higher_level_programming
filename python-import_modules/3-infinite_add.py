8170efd2ebf4e597d5e5cbc4290e41a72bed870e
terrydestroye <s.amuzu@alustudent.com>
2025-02-17 19:48:31 +0200

    3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    sum = 0
+
+    for i, arg enumerate(argv, 1):
+        arg = int(arg)
+        sum = arg
+        sum = sum + arg
+    print(sum)

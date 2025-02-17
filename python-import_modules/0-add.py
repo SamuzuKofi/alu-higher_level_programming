42f781c7fde7c8a013aba3f1891ac991ca67180e
terrydestroye <s.amuzu@alustudent.com>
2025-02-17 14:40:17 +0200

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def add(a,b):
-    a = 1
-    b = 2
+    return a + b
+a = 1
+b = 2
+
+if __name__ == "__main__":  # Ensures code runs only when executed directly
     print("{} + {} = {}".format(a, b, add(a, b)))

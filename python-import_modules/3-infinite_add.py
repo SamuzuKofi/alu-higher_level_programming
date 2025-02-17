5d016c53181150b34527016013d7c9a030d2e5fb
terrydestroye <s.amuzu@alustudent.com>
2025-02-17 19:49:18 +0200

    3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     sum = 0
 
-    for i, arg enumerate(argv, 1):
+    for i, arg in enumerate(argv, 1):
         arg = int(arg)
         sum = arg
         sum = sum + arg

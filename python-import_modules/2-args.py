4ad7a8db70a91983b9dc1a60c7170ae685168978
terrydestroye <s.amuzu@alustudent.com>
2025-02-17 19:36:47 +0200

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     acc_arg = sys.argv[1:]
     arg_len = len(acc_arg)
 
-    print("{} argument{}{}".format(arg_len, "" if arg_len == 1 else "s", ":" if arg_len > 0 else "."))
+    print("{} argument{}{}".format(arg_len, 
+        "" if arg_len == 1 else "s", 
+        ":" if arg_len > 0 else "."))
 
     for i, arg in enumerate(acc_arg, 1):
         print("{}: {}".format(i, arg))

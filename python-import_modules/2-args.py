c539e3100bc5adf045312c800bb8287d28d7df3e
terrydestroye <s.amuzu@alustudent.com>
2025-02-17 19:28:40 +0200

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     acc_arg = sys.argv[1:]
-    arg_len = len(argv)
+    arg_len = len(acc_arg)
 
     print("{} arguments{}{}".format(arg_len, "" if arg_len == 1 else "s", ":" if arg_len > 0 else "."))
 
